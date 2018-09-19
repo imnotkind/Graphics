5b201eab9063b22a8c392911f5f449cd982ac516
linux_sucks <junha1@postech.ac.kr>
2018-09-19 22:42:19 +0900

    test for cuncurrency

--- opengl-test/main.cpp
+++ opengl-test/main.cpp
@@ -11,6 +11,30 @@ void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
 }
+
+
+void mytest1(unsigned char key, int x, int y)
+{
+	if (key == 'q')
+	{
+		cout << "q!" << endl;
+		while (true)
+		{
+			cout << "hihihi" << endl;
+			Sleep(1000);
+		}
+	}
+	cout << "No!" << endl;
+}
+void mytest2(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+	{
+		cout << "hmm??" << endl;
+	}
+	cout << "wtf" << endl;
+}
+
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
@@ -18,6 +42,10 @@ int main(int argc, char **argv) {
 	glutInitWindowSize(320, 320);
 	glutCreateWindow("Hello OpenGL");
 	glutDisplayFunc(renderScene);
+
+	glutKeyboardFunc(mytest1);
+	glutSpecialFunc(mytest2);
+
 	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 	glewInit();
 	glutMainLoop();
