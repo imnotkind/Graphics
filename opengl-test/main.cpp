d962c96a15108952ce8c732cfcbb5ceed2dc6cde
imnotkind <billy307@daum.net>
2018-09-21 13:32:58 +0900

    add glm

--- opengl-test/main.cpp
+++ opengl-test/main.cpp
@@ -2,6 +2,22 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+// Include all GLM core / GLSL features
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+
+// Include all GLM extensions
+#include <glm/ext.hpp> // perspective, translate, rotate
+
+glm::mat4 transform(glm::vec2 const& Orientation, glm::vec3 const& Translate, glm::vec3 const& Up)
+{
+	glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
+	glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
+	glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
+	glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
+	glm::mat4 Model = glm::mat4(1.0f);
+	return Proj * View * Model;
+}
+
 #pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "freeglut.lib")
 #pragma comment(lib, "Opengl32.lib")
