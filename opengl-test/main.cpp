ff43948766b9833e2544c42877dc298a39d9b8e1
imnotkind <billy307@daum.net>
2018-09-21 13:01:45 +0900

    GL is console program

--- opengl-test/main.cpp
+++ opengl-test/main.cpp
@@ -13,19 +13,7 @@ void renderScene(void) {
 	glFlush();
 }
 
-int main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("Hello OpenGL");
-	glutDisplayFunc(renderScene);
-	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-	glewInit();
-	glutMainLoop();
 
-	return 0;
-}
 
 void myReshape(int w, int h) {
 	glLoadIdentity();
@@ -38,4 +26,20 @@ void display() {
 	glColor3f(0.0, 1.0, 1.0);
 	glRectf(10.0, 10.0, 90.0, 90.0);
 	glutSwapBuffers();
+}
+
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(200, 200);
+	glutInitWindowSize(770, 320);
+	glutCreateWindow("Hello OpenGL");
+	glutDisplayFunc(display);
+	glutReshapeFunc(myReshape);
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glewInit();
+	glutMainLoop();
+
+	return 0;
 }
